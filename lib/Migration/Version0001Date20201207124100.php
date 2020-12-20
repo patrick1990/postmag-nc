d1c0af15f382d0e16210219247dc5f88bfb7758a
patrick1990 <patrick1990@users.noreply.github.com>
2020-12-20 11:46:57 +0100

    declare strict types

--- lib/Migration/Version0001Date20201207124100.php
+++ lib/Migration/Version0001Date20201207124100.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace OCA\Postmag\Migration;
 
 use Closure;
--- lib/Service/Errors.php
+++ lib/Service/Errors.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace OCA\Postmag\Service;
 
 use OCP\AppFramework\Db\DoesNotExistException;

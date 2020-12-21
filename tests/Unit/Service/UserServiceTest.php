787161e9231182208d2f4351be3c8cb1fbd98e8e
patrick1990 <patrick1990@users.noreply.github.com>
2020-12-21 21:02:44 +0100

    implement UserService test

---
+++ tests/Unit/Service/UserServiceTest.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+
+namespace OCA\Postmag\Tests\Unit\Service;
+
+use PHPUnit\Framework\TestCase;
+use OCP\IConfig;
+use OCA\Postmag\Db\UserMapper;
+use OCA\Postmag\Service\ConfigService;
+use OCA\Postmag\Service\UserService;
+use OCA\Postmag\Db\User;
+use OCP\AppFramework\Db\DoesNotExistException;
+
+class UserServiceTest extends TestCase {
+    
+    private $service;
+    private $config;
+    private $mapper;
+    private $confService;
+    
+    public function setUp(): void {
+        $this->config = $this->createMock(IConfig::class);
+        $this->mapper = $this->createMock(UserMapper::class);
+        $this->confService = $this->createMock(ConfigService::class);
+        
+        $this->service = new UserService(
+            $this->config,
+            $this->mapper,
+            $this->confService
+        );
+    }
+    
+    public function testGetUserEMail(): void {
+        $userId = 'john';
+        $email = 'john@doe.com';
+        
+        // Mocking
+        $this->config->expects($this->once())
+            ->method('getUserValue')
+            ->with($userId, 'settings', 'email')
+            ->willReturn($email);
+        
+        // Test method
+        $ret = $this->service->getUserEMail($userId);
+        
+        $this->assertEquals($email, $ret, 'Wrong mail address returned.');
+    }
+    
+    public function testGetExistingUserAliasId(): void {
+        $userId = 'john';
+        $userAliasId = '1a2b';
+        
+        $user = new User();
+        $user->setUserId($userId);
+        $user->setUserAliasId($userAliasId);
+        
+        // Mocking
+        $this->mapper->expects($this->once())
+            ->method('findUser')
+            ->with($userId)
+            ->willReturn($user);
+        
+        // Test method
+        $ret = $this->service->getUserAliasId($userId);
+        
+        $this->assertEquals($userAliasId, $ret, 'Wrong user alias id returned');
+    }
+    
+    public function testGetNewUserAliasId(): void {
+        $userId = 'john';
+        
+        // Mocking
+        $this->mapper->expects($this->once())
+            ->method('findUser')
+            ->with($userId)
+            ->willThrowException(new DoesNotExistException('No record found.'));
+            
+        $this->confService->expects($this->any())
+            ->method('getUserAliasIdLen')
+            ->willReturn(ConfigService::DEF_USER_ALIAS_ID_LEN);
+        
+        $this->mapper->expects($this->any())
+            ->method('containsAliasId')
+            ->withAnyParameters()
+            ->willReturn(false);
+        
+        // Test method
+        $ret = $this->service->getUserAliasId($userId);
+        
+        $this->assertEquals('string', gettype($ret), 'user alias is not of type string.');
+        $this->assertEquals(1, preg_match("/^[0-9a-f]*$/", $ret), 'user alias is not a hexadecimal string.');
+        $this->assertEquals(ConfigService::DEF_USER_ALIAS_ID_LEN, strlen($ret), 'user alias is of wrong length.');
+    }
+    
+}

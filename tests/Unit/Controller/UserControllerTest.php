9bd31338389bd23882995e0c5b75cbd801fb5173
patrick1990 <patrick1990@users.noreply.github.com>
2020-12-28 21:29:34 +0100

    implement UserController test

---
+++ tests/Unit/Controller/UserControllerTest.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace OCA\Postmag\Tests\Unit\Controller;
+
+use PHPUnit\Framework\TestCase;
+use OCP\IRequest;
+use OCA\Postmag\Service\UserService;
+use OCA\Postmag\Controller\UserController;
+
+class UserControllerTest extends TestCase {
+    
+    private $controller;
+    private $request;
+    private $service;
+    private $userId = 'john';
+    
+    public function setUp(): void {
+        $this->request = $this->createMock(IRequest::class);
+        $this->service = $this->createMock(UserService::class);
+        
+        $this->controller = new UserController('postmag', $this->request, $this->service, $this->userId);
+    }
+    
+    public function testGetInfoMailSet(): void {
+        // Mocking
+        $this->service->expects($this->once())
+            ->method('getUserEMail')
+            ->with($this->userId)
+            ->willReturn('john@doe.com');
+        
+        $this->service->expects($this->once())
+            ->method('getUserAliasId')
+            ->with($this->userId)
+            ->willReturn('1a2b');
+        
+        // Test method
+        $ret = $this->controller->getInfo();
+        
+        $this->assertSame('john@doe.com', $ret['email'], 'Returned an unexpected mail address.');
+        $this->assertSame('true', $ret['emailSet'], 'Set flag should be true.');
+        $this->assertSame('1a2b', $ret['userAlias'], 'Returned an unexpected user alias id.');
+    }
+    
+    public function testGetInfoMailUnset(): void {
+        // Mocking
+        $this->service->expects($this->once())
+            ->method('getUserEMail')
+            ->with($this->userId)
+            ->willReturn('');
+        
+        $this->service->expects($this->once())
+            ->method('getUserAliasId')
+            ->with($this->userId)
+            ->willReturn('1a2b');
+        
+        // Test method
+        $ret = $this->controller->getInfo();
+        
+        $this->assertSame('', $ret['email'], 'Returned an unexpected mail address.');
+        $this->assertSame('false', $ret['emailSet'], 'Set flag should be false.');
+        $this->assertSame('1a2b', $ret['userAlias'], 'Returned an unexpected user alias id.');
+    }
+    
+}

c9efe9298ca33261d53a505747dc10704f93ce6f
patrick1990 <patrick1990@users.noreply.github.com>
2021-01-04 22:09:58 +0100

    implement UserController integration test

---
+++ tests/Integration/Controller/UserControllerTest.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+namespace OCA\Postmag\Tests\Integration\Controller;
+
+use Test\TestCase;
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\AppFramework\App;
+use OCP\AppFramework\Http\JSONResponse;
+use OCP\AppFramework\Http;
+
+/**
+ * @group DB
+ */
+class UserControllerTest extends TestCase {
+    
+    private $controller;
+    private $mapper;
+    private $userId = 'john';
+    
+    private $userAliasId;
+    
+    public function setUp(): void {
+        parent::setUp();
+        $app = new App('postmag');
+        $container = $app->getContainer();
+        
+        // replace user id
+        $container->registerService('UserId', function ($c) {
+            return $this->userId;
+        });
+        
+        $this->controller = $container->query('OCA\Postmag\Controller\UserController');
+        $this->mapper = $container->query('OCA\Postmag\Db\UserMapper');
+        
+        // Create user alias id of john
+        $this->userAliasId = $this->controller->getInfo()->getData()['userAliasId'];
+    }
+    
+    public function tearDown(): void {
+        // Delete user alias id of john if found
+        $user = $this->mapper->findUser($this->userId);
+        $this->mapper->delete($user);
+        
+        parent::tearDown();
+    }
+    
+    public function testGetInfo(): void {
+        $ret = $this->controller->getInfo();
+        
+        $this->assertTrue($ret instanceof JSONResponse, 'Result should be a JSON response.');
+        $this->assertSame(Http::STATUS_OK, $ret->getStatus(), 'HTTP status should be OK.');
+        $this->assertSame('', $ret->getData()['email'], 'Did not return the expected email address');
+        $this->assertSame('false', $ret->getData()['emailSet'], 'Did not return the expected email set flag');
+        $this->assertSame($this->userAliasId, $ret->getData()['userAliasId'], 'Did not return the expected user alias id');
+    }
+    
+}

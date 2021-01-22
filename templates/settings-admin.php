6e5c7e62a67e9896691a413d023838f18cddd32b
patrick1990 <patrick1990@users.noreply.github.com>
2021-01-22 22:03:59 +0100

    build admin settings form

--- templates/settings-admin.php
+++ templates/settings-admin.php
@@ -1,1 +1,27 @@
-<h1>Hello world</h1>
+<?php
+style('postmag', 'settings-admin');
+?>
+
+<div class="section" id="postmag">
+	<h2><?php p($l->t('Postmag settings')); ?></h2>
+	<p class="settings-hint"><?php p($l->t('Set your target mail domain and alias id lengths.')); ?></p>
+	
+	<label for="postmagDomain"><?php p($l->t('Target mail domain')); ?></label><br/>
+	<input id="postmagDomain"
+		type="text"
+		value="<?php p($_['domain']); ?>"/><br/>
+	
+	<label for="postmagUserAliasIdLen"><?php p($l->t('Length of user alias ids (only for new users)')); ?></label><br/>
+	<input id="postmagUserAliasIdLen"
+		type="number"
+		value="<?php p($_['userAliasIdLen']); ?>"
+		min="<?php p($_['userAliasIdLenMin']); ?>"
+		max="<?php p($_['userAliasIdLenMax']); ?>"/><br/>
+	
+	<label for="postmagAliasIdLen"><?php p($l->t('Length of alias ids (only for new aliases)')); ?></label><br/>
+	<input id="postmagAliasIdLen"
+		type="number"
+		value="<?php p($_['aliasIdLen']); ?>"
+		min="<?php p($_['aliasIdLenMin']); ?>"
+		max="<?php p($_['aliasIdLenMax']); ?>"/><br/>
+</div>

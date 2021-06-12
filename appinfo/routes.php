1a0fb0d6d5f6ffe1baf6a5dfdb31755c03285388
patrick1990 <patrick1990@users.noreply.github.com>
2021-06-13 00:13:50 +0200

    updated routes file for alias read endpoint

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -15,6 +15,7 @@ return [
         ['name' => 'user#getinfo',   'url' => '/userinfo',   'verb' => 'GET'],    // get user info
 	    ['name' => 'alias#index',    'url' => '/alias',      'verb' => 'GET'],    // return a list of all aliases
         ['name' => 'alias#create',   'url' => '/alias',      'verb' => 'POST'],   // create a new alias
+        ['name' => 'alias#read',     'url' => '/alias/{id}', 'verb' => 'GET'],    // get a specified alias
         ['name' => 'alias#update',   'url' => '/alias/{id}', 'verb' => 'PUT'],    // update alias properties
         ['name' => 'alias#delete',   'url' => '/alias/{id}', 'verb' => 'DELETE'], // delete an alias
     ]

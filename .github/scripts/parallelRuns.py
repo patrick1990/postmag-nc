35ec2a4079c19234b3d1880f3469a781918a3f59
patrick1990 <patrick1990@users.noreply.github.com>
2021-08-30 15:08:25 +0200

    Prevent parallel Github Actions workflow runs (#49)
    * add python script to check for parallel workflow runs
    
    * add queued workflows to parallel run check
    
    * add some debug info to parallel run check
    
    * add parallel run check to semantic label check
    
    * add parallel run check to nc version pr workflow
    
    * add parallel run check to app package workflow
    
    * add parallel run check to release cycle workflow
    
    * add parallel run check to app test workflow
    
    * get sha from workflows api

---
+++ .github/scripts/parallelRuns.py
@@ -0,0 +1,73 @@
+#
+# @author Patrick Greyson
+#
+# Postmag - Postfix mail alias generator for Nextcloud
+# Copyright (C) 2021
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+import requests
+import time
+
+# Get arguments
+token = sys.argv[1]
+
+# Some configs
+runEndpoint = "https://api.github.com/repos/" + os.environ["GITHUB_REPOSITORY"] + "/actions/runs/" + str(os.environ["GITHUB_RUN_ID"])
+workflowEndpoint = "https://api.github.com/repos/" + os.environ["GITHUB_REPOSITORY"] + "/actions/workflows/" + os.environ["GITHUB_WORKFLOW"] +".yml/runs"
+headers = {
+  "Accept": "application/vnd.github.v3+json",
+  "Authorization": "Bearer " + token
+}
+
+# Sleep a little bit to wait for parallel runs to start
+sys.stderr.write("Wait 30s for other potential runs...\n")
+time.sleep(30)
+
+# Since GITHUB_SHA holds the sha of the merge branch for pull requests, we get the head sha from the workflows api
+response = requests.get(runEndpoint, headers=headers)
+if response.status_code != 200:
+  # No successful response --> Error
+  sys.stderr.write("Got no successful response from Github API to get workflow run.\n")
+  sys.exit(1)
+mySHA = str(response.json()["head_sha"])
+
+# Search for parallel runs
+for status in ["queued", "in_progress"]:
+  page = 0
+  workflows = [None]
+  while len(workflows) != 0:
+    page = page+1
+    response = requests.get(workflowEndpoint + "?status=" + status + "&page=" + str(page), headers=headers)
+    if response.status_code != 200:
+      # No successful response --> Error
+      sys.stderr.write("Got no successful response from Github API to list workflows.\n")
+      sys.exit(1)
+
+    workflows = response.json()["workflow_runs"]
+    sys.stderr.write(" - Me    #" + str(os.environ["GITHUB_RUN_NUMBER"]) + ": SHA " + mySHA + "\n")
+    for workflow in workflows:
+      sys.stderr.write(" - Other #" + str(workflow["run_number"]) + ": SHA " + str(workflow["head_sha"]) + "\n")
+      if str(workflow["head_sha"]) == mySHA:
+        # Parallel run found. Stop me if my run number is higher.
+        if int(workflow["run_number"]) < int(os.environ["GITHUB_RUN_NUMBER"]):
+          sys.stderr.write("Parallel run with smaller run number found. Stop me!\n")
+          sys.stdout.write("True")
+          sys.exit(0)
+
+sys.stderr.write("No parallel runs found (Or I'm the first run).\n")
+sys.stdout.write("False")
+sys.exit(0)
